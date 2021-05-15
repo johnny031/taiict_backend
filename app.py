b7f472d0a3ab2d7c507a8c26b61dbe85536a7f0c
Johnny <johnny055238@gmail.com>
2021-05-16 00:32:20 +0800

    test

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host = "us-cdbr-east-03.cleardb.com",
     user = "bffbfcd0c09f06",
-    passwd = "a3415195"
+    passwd = "a3415195",
+    database = "heroku_d6918b07f609b9c"
 )
 
 cursor = db.cursor()
 
-cursor.execute("CREATE DATABASE testdatabase")
-
 # cursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, persionID int PRIMARY KEY AUTO_INCREMENT)")
 
-# cursor.execute("INSERT INTO Person (name, age) VALUES (%s, %s)", ("Joe", 23))
+# cursor.execute("INSERT INTO Person (name, age) VALUES (%s, %s)", ("John", 23))
 # db.commit()
 
-# cursor.execute("SELECT * FROM Person")
+cursor.execute("SELECT * FROM Person")
 
-# for x in cursor:
-#     print(x)
+for x in cursor:
+    print(x)
 
 @app.route('/')
 def hello():
-    return f'Hello, Heroku!'
+    for x in cursor:
+        return f'Hello, Heroku! {x}'
 
 if __name__ == 'main':
     app.run()
