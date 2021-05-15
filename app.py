32aa885b3a61434970afb281ac27c842e3548aa3
Johnny <johnny055238@gmail.com>
2021-05-16 00:14:04 +0800

    test

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-import mysql.connector
 from flask import Flask, request
+import os
+
+import pymysql
+import pymysql.cursors
+
 
 app = Flask(__name__)
 
+# Connect to the database
+connection = pymysql.connect(host=os.environ.get('us-cdbr-east-03.cleardb.com'),
+                             user=os.environ.get('bffbfcd0c09f06'),
+                             password=os.environ.get('a3415195'),
+                             db=os.environ.get('heroku_d6918b07f609b9c'),
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
-db = mysql.connector.connect(
-    host = "us-cdbr-east-03.cleardb.com",
-    user = "bffbfcd0c09f06",
-    passwd = "a3415195",
-    database = "testdatabase"
-)
+with connection.cursor() as cursor:
+    # Create a new record
+    sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+    cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
-cursor = db.cursor()
+connection.commit()
 
-cursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, persionID int PRIMARY KEY AUTO_INCREMENT)")
-
-cursor.execute("INSERT INTO Person (name, age) VALUES (%s, %s)", ("Joe", 23))
-db.commit()
-
-cursor.execute("SELECT * FROM Person")
-
-# for x in cursor:
-#     print(x)
 
 @app.route('/')
 def hello():
-    return f'Hello, Heroku!'
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id`, `email` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
+    return f'Hello, Heroku {result["email"]}!'
 
 if __name__ == 'main':
-    app.run()
+
+    app.run() #啟動伺服器
