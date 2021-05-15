ade24b1eb1e3b0a5322598859eff00ab5f1bc705
Johnny <johnny055238@gmail.com>
2021-05-16 00:23:27 +0800

    test

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ db = mysql.connector.connect(
     passwd = "a3415195"
 )
 
-# cursor = db.cursor()
+cursor = db.cursor()
+
+cursor.execute("CREATE DATABASE testdatabase")
 
 # cursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, persionID int PRIMARY KEY AUTO_INCREMENT)")
 
