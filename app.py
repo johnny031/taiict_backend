c3e76c0ccab4fbef59a2dd589e2b7b3076216fd1
Johnny <johnny055238@gmail.com>
2021-05-16 13:23:39 +0800

    add note to database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import mysql.connector
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -13,20 +14,44 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-# cursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, persionID int PRIMARY KEY AUTO_INCREMENT)")
-
-# cursor.execute("INSERT INTO Person (name, age) VALUES (%s, %s)", ("John", 23))
+# cursor.execute("DROP TABLE Note")
+# cursor.execute("CREATE TABLE Note (note VARCHAR(50),name VARCHAR(50), noteID int PRIMARY KEY AUTO_INCREMENT)")
+# cursor.execute("INSERT INTO Note (note, name) VALUES (%s, %s)", ("First note", "John"))
+# cursor.execute("INSERT INTO Note (note, name) VALUES (%s, %s)", ("Second note", "Joe"))
+# cursor.execute("INSERT INTO Note (note, name) VALUES (%s, %s)", ("Third note", "Mary"))
 # db.commit()
 
-cursor.execute("SELECT * FROM Person")
+# cursor.execute("SELECT * FROM Note")
 
-list = []
-for x in cursor:
-    list.append(x)
+# list = []
+# for x in cursor:
+#     list.append(x)
+# print(list)
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def hello():
-    return f'Hello, Heroku! {list[0]}'
+    if request.method == "POST":
+        note = request.form.get("note")
+        cursor.execute("INSERT INTO Note (note, name) VALUES (%s, %s)", (note, "John"))
+        db.commit()
+
+    cursor.execute("SELECT * FROM Note")
+    list = []
+    for x in cursor:
+        list.append(x)
+    # print(list)
+    return render_template("index.html", list=list)
+
+@app.route('/delete-note', methods=["POST"])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note["noteId"]
+    
+    cursor.execute(f"DELETE FROM Note WHERE noteId = {noteId}")
+    db.commit()
+
+    return jsonify({})
 
 if __name__ == 'main':
     app.run()
