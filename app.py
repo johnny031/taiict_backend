55803a6518cd04bac6171f47bc6f6dd1c3fdfd30
Johnny <johnny055238@gmail.com>
2021-05-22 21:29:51 +0800

    test

--- app.py
+++ app.py
@@ -57,17 +57,17 @@ class Img(db.Model):
     width = db.Column(db.String(5), nullable=False)
     news_Id = db.Column(db.Integer, db.ForeignKey("news.newsId"), nullable=False)
 
-# Img.__table__.drop(db.engine)
+# News.__table__.drop(db.engine)
 # new_news = News(author="author", title="title", content="content")
 # db.session.add(new_news)
 # News.query.filter(News.newsId == 249).delete()
 # db.session.commit()
 
-# news_del = News.query.filter_by(newsId=261).first()
+# news_del = News.query.filter_by(newsId=264).first()
 # db.session.delete(news_del)
 # db.session.commit()
 
-# news = Img.query.all()
+# news = News.query.all()
 # print(news)
 
 @login_manager.user_loader
