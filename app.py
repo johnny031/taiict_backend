a4ef7d5474b8a7e50e09004325acbf085b689a1f
Johnny031 <johnny055238@gmail.com>
2022-07-31 00:18:21 +0800

    change database to cleardb

--- app.py
+++ app.py
@@ -36,6 +36,29 @@ login_manager.init_app(app)
 login_manager.login_view = "login.user_login"
 login_manager.login_message = "您沒有權限，請先登入"
 
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+db = SQLAlchemy(app)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    premium = db.Column(db.Boolean(), nullable=False)
+class News(db.Model):
+    newsId = db.Column(db.Integer, primary_key = True)
+    author = db.Column(db.String(50), nullable=False)
+    datetime = db.Column(db.String(50), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(3000), nullable=False)
+    files = db.relationship("File", cascade="all,delete", backref="news")
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable=False)
+    news_Id = db.Column(db.Integer, db.ForeignKey("news.newsId"))
+db.create_all()
+
+
 @login_manager.user_loader
 def load_user(id):
     user = User.query.get(id)
